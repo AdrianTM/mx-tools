46fe2c859c8e25a811351ecf82e76680077a8a10
Adrian <adrian@mxlinux.org>
2014-11-20 08:34:24 -0500

    added IT and CA translations

---
+++ translations/mx-tools_it.ts
@@ -0,0 +1,121 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.0">
+<context>
+    <name>mxtools</name>
+    <message>
+        <location filename="../mxtools.ui" line="14"/>
+        <location filename="../mxtools.cpp" line="103"/>
+        <source>MX Tools</source>
+        <translation>MX Strumenti</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="35"/>
+        <source>About this application</source>
+        <translation>A proposito di questa applicazione</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="38"/>
+        <source>About...</source>
+        <translation>A proposito...</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="45"/>
+        <source>Alt+B</source>
+        <translation>Alt+B</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="103"/>
+        <source>Cancel any changes then quit</source>
+        <translation>Cancella tutte le modifiche e poi esci</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="106"/>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="113"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="158"/>
+        <source>Check Apt GPG</source>
+        <translation>Controllare Apt GPG</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="169"/>
+        <source>Find Network Shares</source>
+        <translation>Trova Reti Condivise</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="180"/>
+        <source>Package Installer</source>
+        <translation>Installer Pacchetti</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="191"/>
+        <source>Unhide Apt Notifier</source>
+        <translation>Mostra Apt Notifier</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="202"/>
+        <source>User Manager</source>
+        <translation>Manager Utenti</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="213"/>
+        <source>Flash Manager</source>
+        <translation>Flash Manager</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="224"/>
+        <source>Switch User</source>
+        <translation>Cambio Utente</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="235"/>
+        <source>Boot Repair</source>
+        <translation>Riparazione Boot</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="246"/>
+        <source>Codecs Downloader</source>
+        <translation>Codecs Downloader</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.ui" line="269"/>
+        <source>These MX applications save time and effort with important tasks.</source>
+        <translation>Queste applicazioni di MX fanno risparmiare tempo e fatica in attività importanti.</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.cpp" line="102"/>
+        <source>About MX Tools</source>
+        <translation>Riaguardo a MX Strumenti</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.cpp" line="103"/>
+        <source>Version: </source>
+        <translation>Versione:</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.cpp" line="105"/>
+        <source>Configuration Tools for MX Linux</source>
+        <translation>Configurazione degli strumenti per MX Linux</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.cpp" line="106"/>
+        <source>Copyright (c) antiX</source>
+        <translation>Copyright (c) antiX</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.cpp" line="107"/>
+        <source>License</source>
+        <translation>Licenza</translation>
+    </message>
+    <message>
+        <location filename="../mxtools.cpp" line="108"/>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+</context>
+</TS>
