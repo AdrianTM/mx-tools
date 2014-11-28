02a5df11298dc0745de00792609345a74cd40909
Adrian <adrian@mxlinux.org>
2014-11-28 17:59:40 -0500

    accept arguments

--- mx-tools.sh
+++ mx-tools.sh
@@ -2,8 +2,7 @@
 DESKTOP_PATH="/usr/share/applications"
 EXTRA_FILES=""
 #EXTRA_FILES="synaptic.desktop"
-EXTRA_PASSED=""
-#EXTRA_PASSED="$@ |sed \"s/$1//ig\""
+EXTRA_PASSED="${@:2}"
 
 help() {
     echo "This program is to show and hide all of the mx-* apps (accept mx-tools) from the menu"
@@ -19,46 +18,47 @@ help() {
 }
 
 SHOW_HIDE() {
-    for DESKTOP_FILE in $(ls -1 $DESKTOP_PATH/{,mx/}mx*.desktop | grep -v mx-tools) $EXTRA_FILES $EXTRA_PASSED
+    for DESKTOP_FILE in $(ls -1 $DESKTOP_PATH/{,mx/}mx*.desktop | grep -v mx-tools) $DESKTOP_PATH/$EXTRA_FILES $EXTRA_PASSED
     do
-       OLD_VALUE=$(cat $DESKTOP_FILE | grep "^NoDisplay=" | cut -d "=" -f2)
-       case "$OLD_VALUE" in
-          true)
-              if [ "$CONDITION" != "hide" ]; then
-                  sed -i "s/^NoDisplay=.*/NoDisplay=false/" $DESKTOP_FILE
-              fi
-          ;;
-          false)
-              if [ "$CONDITION" != "show" ]; then
-                  sed -i "s/^NoDisplay=.*/NoDisplay=true/" $DESKTOP_FILE
-              fi
-          ;;
-          *)
-             sed -i -e '$a\' $DESKTOP_FILE
-             if [ "$CONDITION" = "hide" ]; then
-                 echo "NoDisplay=true" >> $DESKTOP_FILE
-             else [ "$CONDITION" = "show" ];
-                 echo "NoDisplay=false" >> $DESKTOP_FILE
-             fi
-          ;;
-       esac
+        if [ -f $DESKTOP_FILE ]; then
+            OLD_VALUE=$(cat $DESKTOP_FILE | grep "^NoDisplay=" | cut -d "=" -f2)
+            case "$OLD_VALUE" in
+                true)
+                    if [ "$CONDITION" != "hide" ]; then
+                        sed -i "s/^NoDisplay=.*/NoDisplay=false/" $DESKTOP_FILE
+                    fi
+                ;;
+                false)
+                    if [ "$CONDITION" != "show" ]; then
+                        sed -i "s/^NoDisplay=.*/NoDisplay=true/" $DESKTOP_FILE
+                    fi
+                ;;
+                *)
+                    sed -i -e '$a\' $DESKTOP_FILE
+                    if [ "$CONDITION" = "hide" ]; then
+                        echo "NoDisplay=true" >> $DESKTOP_FILE
+                        else [ "$CONDITION" = "show" ];
+                        echo "NoDisplay=false" >> $DESKTOP_FILE
+                    fi
+                ;;
+            esac
+        fi
     done
 }
 
 case $1 in
     -h|--hide)
         CONDITION="hide"
-	SHOW_HIDE
+        SHOW_HIDE
     ;;
     -s|--show)
         CONDITION="show"
-	SHOW_HIDE
+        SHOW_HIDE
     ;;
     -H|--help)
-      help;
+        help;
    ;;
    *)
-       help;
+        help;
    ;;
 esac
-
