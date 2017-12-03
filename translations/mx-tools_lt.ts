48d9455c3dc192a725c33d47146e77ac7a08ba01
oldgizahub <geoffgigg@hotmail.com>
2017-12-03 13:24:11 -0500

    updated desktop and lt translation for mx-tools

--- translations/mx-tools_lt.ts
+++ translations/mx-tools_lt.ts
@@ -1,13 +1,11 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="lt">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
 <context>
     <name>mxtools</name>
     <message>
         <location filename="../mxtools.ui" line="14"/>
-        <location filename="../mxtools.cpp" line="312"/>
+        <location filename="../mxtools.cpp" line="306"/>
         <source>MX Tools</source>
-        <translation>MX Įrankiai</translation>
+        <translation>MX įrankiai</translation>
     </message>
     <message>
         <location filename="../mxtools.ui" line="47"/>
@@ -20,72 +18,72 @@
         <translation>Apie...</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="58"/>
+        <location filename="../mxtools.ui" line="56"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="116"/>
+        <location filename="../mxtools.ui" line="114"/>
         <source>Close application</source>
         <translation>Užverti programą</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="119"/>
+        <location filename="../mxtools.ui" line="117"/>
         <source>Close</source>
         <translation>Užverti</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="127"/>
+        <location filename="../mxtools.ui" line="123"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="146"/>
+        <location filename="../mxtools.ui" line="142"/>
         <source>Manual</source>
         <translation>Vadovas</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="242"/>
+        <location filename="../mxtools.ui" line="236"/>
         <source>These MX applications save time and effort with important tasks.</source>
         <translation>Šios MX programos sutaupo laiko ir pastangų, atliekant svarbias užduotis.</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="258"/>
+        <location filename="../mxtools.ui" line="252"/>
         <source>Hide individual tools from the menu</source>
         <translation>Slėpti atskirus įrankius iš meniu</translation>
     </message>
     <message>
-        <location filename="../mxtools.ui" line="277"/>
+        <location filename="../mxtools.ui" line="271"/>
         <source>search</source>
         <translation>ieškoti</translation>
     </message>
     <message>
-        <location filename="../mxtools.cpp" line="311"/>
+        <location filename="../mxtools.cpp" line="305"/>
         <source>About MX Tools</source>
-        <translation>Apie MX Įrankiai</translation>
+        <translation>Apie MX įrankiai</translation>
     </message>
     <message>
-        <location filename="../mxtools.cpp" line="312"/>
+        <location filename="../mxtools.cpp" line="306"/>
         <source>Version: </source>
         <translation>Versija: </translation>
     </message>
     <message>
-        <location filename="../mxtools.cpp" line="314"/>
+        <location filename="../mxtools.cpp" line="308"/>
         <source>Configuration Tools for MX Linux</source>
         <translation>Konfigūravimo įrankiai, skirti MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxtools.cpp" line="315"/>
+        <location filename="../mxtools.cpp" line="309"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Autorių teisės (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxtools.cpp" line="316"/>
+        <location filename="../mxtools.cpp" line="310"/>
         <source>License</source>
         <translation>Licencija</translation>
     </message>
     <message>
-        <location filename="../mxtools.cpp" line="317"/>
+        <location filename="../mxtools.cpp" line="311"/>
         <source>Cancel</source>
         <translation>Atsisakyti</translation>
     </message>
