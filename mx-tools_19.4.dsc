5b817fb7269c32fe2af5823e2ed3c06b727ec170
Adrian <adrian@mxlinux.org>
2019-04-05 12:01:06 -0400

    update translations

---
+++ mx-tools_19.4.dsc
@@ -0,0 +1,32 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Format: 3.0 (native)
+Source: mx-tools
+Binary: mx-tools
+Architecture: any
+Version: 19.4
+Maintainer: Adrian <adrian@mxlinux.org>
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/AdrianTM/mx-tools
+Build-Depends: qt5-qmake, qt5-default, qttools5-dev-tools, debhelper (>= 9)
+Package-List:
+ mx-tools deb admin optional arch=any
+Checksums-Sha1:
+ 835bae878606fb07319ccd937324994dcbe9a3c8 34660 mx-tools_19.4.tar.xz
+Checksums-Sha256:
+ 24a3e403282e6ec44792ecbed94099355732ced850c9f98694158e8ada83b3d5 34660 mx-tools_19.4.tar.xz
+Files:
+ e5156b154faa6c964f759449e5865b3d 34660 mx-tools_19.4.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCAAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAlyne6oTHGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumBs2B/4kOCw+siX5MrJ4/Ct4AMpWY4MYK/V0
+68+kzX2d9tZZlrvZrrB6+30+ISegLwd/U67IAbZG7TSAJJUmepZ4G3Jc4EozV6DF
+XrxHMrIH2ZqWB/nMsm/Rg/5qBzCslGPQjxFUQ+r/u13M7uvIwcA76ViwkV1nYaNs
+vbhFKWvqWZfId1PQ4JaxQy2/vBk/lNGvLFG5tCoBuawwoLE4ePp/Dt200lxFH5/y
+UajBVJyP+qkPwJP3Gmh8t+BulVE1s0x4CCJsAXX8DBUPnEJoYprFWyCOSODzbfWn
+m+VjZvUjMGYLba9r7TEwgNvhnEWVCZkmY8rB8SiYXsAuEVZDSO35NpMR
+=IvOd
+-----END PGP SIGNATURE-----
--- translations/mx-tools_sk.ts
+++ translations/mx-tools_sk.ts
@@ -1,99 +1,101 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
 <context>
     <name>mxtools</name>
     <message>
-        <location filename="mxtools.ui" line="14"/>
-        <location filename="mxtools.cpp" line="334"/>
+        <location filename="../mxtools.ui" line="14"/>
+        <location filename="../mxtools.cpp" line="365"/>
         <source>MX Tools</source>
         <translation>MX Tools</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="47"/>
+        <location filename="../mxtools.ui" line="47"/>
         <source>About this application</source>
         <translation>O tejto aplikácii</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="50"/>
+        <location filename="../mxtools.ui" line="50"/>
         <source>About...</source>
         <translation>O Programe</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="58"/>
+        <location filename="../mxtools.ui" line="58"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="116"/>
+        <location filename="../mxtools.ui" line="116"/>
         <source>Close application</source>
         <translation>Zatvoriť aplikáciu</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="119"/>
+        <location filename="../mxtools.ui" line="119"/>
         <source>Close</source>
         <translation>Zatvoriť</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="127"/>
+        <location filename="../mxtools.ui" line="127"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="149"/>
+        <location filename="../mxtools.ui" line="149"/>
         <source>Manual</source>
         <translation>Užívateľský manuál</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="248"/>
+        <location filename="../mxtools.ui" line="248"/>
         <source>These MX applications save time and effort with important tasks.</source>
         <translation>Tieto aplikácie zkracujú čas a zmenšujú námahu s dôležitými úlohami. </translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="264"/>
+        <location filename="../mxtools.ui" line="264"/>
         <source>Hide individual tools from the menu</source>
         <translation>Schovať individuálne položky v hlavnej ponuke</translation>
     </message>
     <message>
-        <location filename="mxtools.ui" line="283"/>
+        <location filename="../mxtools.ui" line="283"/>
         <source>search</source>
         <translation>hľadať</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="333"/>
+        <location filename="../mxtools.cpp" line="364"/>
         <source>About MX Tools</source>
         <translation>O programe MX Tools</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="334"/>
+        <location filename="../mxtools.cpp" line="365"/>
         <source>Version: </source>
         <translation>Verzia</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="336"/>
+        <location filename="../mxtools.cpp" line="367"/>
         <source>Configuration Tools for MX Linux</source>
         <translation>Konfiguračné nástroje MX Linux</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="337"/>
+        <location filename="../mxtools.cpp" line="368"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="338"/>
+        <location filename="../mxtools.cpp" line="369"/>
         <source>License</source>
         <translation>Licencia</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="339"/>
+        <location filename="../mxtools.cpp" line="370"/>
         <source>Changelog</source>
         <translation>História zmien</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="340"/>
+        <location filename="../mxtools.cpp" line="371"/>
         <source>Cancel</source>
         <translation>Zrušiť</translation>
     </message>
     <message>
-        <location filename="mxtools.cpp" line="355"/>
+        <location filename="../mxtools.cpp" line="386"/>
         <source>&amp;Close</source>
         <translation>&amp;Zatvoriť</translation>
     </message>
